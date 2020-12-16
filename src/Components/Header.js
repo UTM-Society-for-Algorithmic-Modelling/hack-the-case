f0edf974ce4cadfad72c097515dee4b726ed9100
Brian <brianli.li@mail.utoronto.ca>
2020-12-16 14:04:26 -0500

    video not working

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import logo from "../logo.svg";
-import background from "./Hackathon.jpg";
+import background from "./Hackathon.mp4";
 export default function Header({ title, subtitle, dark, id }) {
   return (
       <div id={id}>
