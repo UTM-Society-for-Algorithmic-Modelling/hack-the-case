116652e74dcfb9905711af68ed7edbe70ed03a8e
Zain Kazmi <zain.kazmi@utoronto.ca>
2021-01-23 22:42:49 -0500

    Add HTC partner logos

---
+++ src/Components/HTCpartners.js
@@ -0,0 +1,28 @@
+import React from "react";
+import MCSS from "./MCSS.png";
+import WISC from "./WiSC_Transparent_Logo_1.png";
+import UTMSAM from "./UTMSAM_transparent_without_chaos.png";
+import DSC from "./DSC.png";
+
+export default function HTCPartners({ title, subtitle, dark, id }) {
+  const image = {
+	justifyContent: "flex-start",
+	width: "200px",
+	height: "200px",
+  };
+  return (
+    <div className={"section" + (dark ? " section-dark" : "")}>
+      <div className="section-content" id={id}>
+        <h1>{title}</h1>
+	<div style={{display: "flex"}}> 
+	<img style={image} src={MCSS} />
+	<img style={image} src={WISC} />
+	<img style={image} src={UTMSAM} />
+	<img style={image} src={DSC} />
+        </div>  
+      </div>
+    </div>
+  );
+}
+
+
