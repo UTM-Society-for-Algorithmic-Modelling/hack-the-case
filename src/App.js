9505108efe42292baebcabadc2636b6f40b2c4c1
Brian <brianli.li@mail.utoronto.ca>
2021-01-23 22:48:36 -0500

    text added

--- src/App.js
+++ src/App.js
@@ -19,19 +19,19 @@ class App extends Component {
         />
         <Registration
           title="Registration Form"
-          subtitle="Please enter your information Below"
+          subtitle="Please apply below. Your team should consist 3-4 members preferably from different backgrounds as the team should be competent in both hackathon and case competition. If you have questions or inquires, please email sam@utmsu.ca"
           dark={false}
           id="section2"
         />
         <Section
-          title="About HTC"
-          subtitle="Welcome to Hack the Case. Imagine a hackathon, case competition, workshops, and game events mashed together with networking opportunities, and tons of prizes. That's us. Hack the Case is an innovation focused competition designed for diversed students of all backgrounds."	    
+          title="About Hack The Case"
+          subtitle="Welcome to Hack the Case! Imagine a hackathon, case competition, workshops, and game events mashed together with networking opportunities, and tons of prizes. That's us! Hack the Case is an innovation focused competition designed for diversed students of all backgrounds."	    
           dark={true}
           id="section3"
         />
         <Section
-          title="HTC Information"
-          subtitle={dummyText}
+          title="Hack The Case Information"
+          subtitle="The competition will start on Febuary 15 and end on Febuary 22. However, you are not required to be present throughout the week. We will soon reveal a detailed schedules with specific times for all subevents. During the week, Hack the Case will be hosting different workshops, mentorship, game nights, and networking opportunities. We also have awesome prizes prepared for the participants and winners, stay tuned for more updates!"
           dark={false}
           id="section4"
         />
--- src/Components/Registration.js
+++ src/Components/Registration.js
@@ -14,8 +14,6 @@ export default function Registration({ title, subtitle, dark, id }) {
 return (
     <div className={"section" + (dark ? " section-dark" : "")}>
       <div className="section-content" id={id}>
-	<h3>Registration and Teams: Each team consists of 4 members, preferably from different backgrounds as the team should be competent in both hackathon and case competition. If you don’t have a team yet, don’t worry! You can find your teammates in the <a href="https://discord.gg/h4TgEgN" target = "external">discord channel!</a>.
-	  </h3>
 	<h1>{title}</h1>
         <p>{subtitle}</p>
 		<div>
